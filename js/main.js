cbfb06a9e135fb2cd316e888fafad08f256d4261
Gabi <gabrielajustinodesouza@gmail.com>
2024-02-26 11:11:40 -0300

    pequena alteração no carousel de imagens

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function(){   
+    var nav = $('#menu-fixo');   
+    $(window).scroll(function () { 
+        if ($(this).scrollTop() > 170) { 
+            nav.addClass("menu-fixo");
+            $('body').css('padding-top', 70);
+            $("#hr-menu-fixo").remove();
+        } else { 
+            nav.removeClass("menu-fixo"); 
+            $('body').css('padding-top', 0);
+        } 
+    });  
+}); 
+
+    
+    
+function adicionaOuRemoveClasseResponsiva() {
+    var res = $('.s-res');
+    if($(this).width() <= 480){
+        res.addClass("slider-item-reveal");
+    } else {
+        res.removeClass('slider-item-reveal');
+    }
+}
+
+window.addEventListener('resize', adicionaOuRemoveClasseResponsiva);
+adicionaOuRemoveClasseResponsiva();
